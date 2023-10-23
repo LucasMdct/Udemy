5ad72cccbabd1ef216f4bf28588ce0f07221a4ae
˜LucasMdct˜ <lucasmedeiroscosta07@gmail.com>
2023-10-23 11:16:12 -0300

    class 28

---
+++ OtavioMiranda/JSandTY-basic-to-advanced/JS/Classes/Class_28/index.js
@@ -0,0 +1,9 @@
+
+
+
+// const tresHoras = 60 * 60 * 3 * 1000;
+// const umDia = 60 * 60 * 24 * 1000;
+// const data = new Date(0 + tresHoras + umDia);
+
+const data = new Date();
+console.log(data.toString());
