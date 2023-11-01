3b91e251b61cb0136463f76317d6963444a63b58
˜LucasMdct˜ <lucasmedeiroscosta07@gmail.com>
2023-11-01 10:40:14 -0300

    Section 3 - JS - LOGICA DA PROGRAMACAO

---
+++ OtavioMiranda/JSandTY-basic-to-advanced/JS/JS_Basico_e_Logica_Programacao/Class_31/index.js
@@ -0,0 +1,55 @@
+
+// const verdadeira = true;
+
+// let nome = "Lucas"; // criando 
+// var nome2 = "Lucas2";
+
+//let nome = "TESTE"; // erro nao podemos reanomear a variavel
+
+//et nome2 = "lucas2"; // podemos substituir a var 
+
+//LET TEM UM ESCOPO DE BLOCO { ... BLOCO }
+
+// {
+//     let nome = "lUCAS2";
+//     console.log(nome, nome2);
+// }
+
+// VAR SO TEM UM ESCOPO DE FUNCAO
+
+// var nome2 = "Medeiros";
+
+// if(verdadeira) {
+//     let nome = "lUCAS"; // criando outra que se chama nome dentro desse bloco
+
+//     var nome2 = 'Medeiros';// redeclarando
+    
+//     if(verdadeira) {
+//         let nome = "Outra Coisa";
+//         let nome2 = "Andressa";
+//     }
+// }
+
+// console.log(nome,nome2); 
+
+
+// function falaOi () {
+//     var sobrenome = "Medeiros";
+
+//     if(true) {
+//         let nome = 'Lucas';
+//         console.log(sobrenome);
+//         console.log(nome);
+//     }
+//     // console.log('Oi');
+//     // var nome = "Lucas";
+//     // console.log(sobrenome);
+// }
+
+// falaOi();
+let sobrenome = 'Lucas';
+console.log(sobrenome);
+
+//var sobrenome = 'Lucas';
+
+
