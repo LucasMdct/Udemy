1831de04f88b13eae8f5ec4b50b3592538d77a6d
˜LucasMdct˜ <lucasmedeiroscosta07@gmail.com>
2023-12-13 15:15:13 -0300

    Update Course - Class 271.  Type ANY

---
+++ OtavioMiranda/JSandTY-basic-to-advanced/TypeScript/S19-BasicTypes/270-SintaxeType/src/C0002-type-any.ts
@@ -0,0 +1,8 @@
+// type ANY use as a last resort
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3, 4]));
+console.log(showMessage('Hello'));
+console.log(showMessage(1));
