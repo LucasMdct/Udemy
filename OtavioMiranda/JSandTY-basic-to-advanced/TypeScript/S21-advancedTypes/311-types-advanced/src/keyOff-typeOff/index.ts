4a61ed35d6fc9be2a193db2266e53706674e9ef0
˜LucasMdct˜ <lucasmedeiroscosta07@gmail.com>
2023-12-18 16:41:23 -0300

    Update TS Class 312 -  key off and type off

---
+++ OtavioMiranda/JSandTY-basic-to-advanced/TypeScript/S21-advancedTypes/311-types-advanced/src/keyOff-typeOff/index.ts
@@ -0,0 +1,17 @@
+type ColorsObj = typeof colorsObj;
+type KeyColors = keyof ColorsObj;
+
+const colorsObj = {
+  red: 'vermelho',
+  green: 'verde',
+  blue: 'azul',
+  purple: 'roxo',
+};
+
+function translateColor(color: KeyColors, colors: ColorsObj) {
+  return colors[color];
+}
+
+console.log(translateColor('red', colorsObj));
+console.log(translateColor('green', colorsObj));
+console.log(translateColor('purple', colorsObj));
