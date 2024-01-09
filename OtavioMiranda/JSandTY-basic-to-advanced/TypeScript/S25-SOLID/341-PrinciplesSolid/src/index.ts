9837d286c982c5740a58fe6bde5f77120f32e663
˜LucasMdct˜ <lucasmedeiroscosta07@gmail.com>
2024-01-09 10:28:37 -0300

    Update TS Class 352. ADVANTAGES AND DISADVANTAGES - DRY - YAGNI  - KISS - SOC - MVP

--- OtavioMiranda/JSandTY-basic-to-advanced/TypeScript/S25-SOLID/341-PrinciplesSolid/src/index.ts
+++ OtavioMiranda/JSandTY-basic-to-advanced/TypeScript/S25-SOLID/341-PrinciplesSolid/src/index.ts
@@ -1,3 +1,20 @@
-const nameConst = 'lUCAS';
+/*
+VANTAGENS E DESVANTAGENS DO S.O.L.I.D
 
-console.log(nameConst);
+vantagens:
+- Código modular.
+- Código reutilizável ( D.R.Y - Don't reapet yourself).
+- Código testavel, baixo acomplamento.
+- Baixo acoplamento e alta coesäo.
+- Código expsansível.
+- Separations of concerns (separaçao de conceitows)
+- Fácil manuntencao
+
+desvantagens:
+- Complexidade
+- Quantidade de código digitado aumenta.
+- Tempo de desenvolvimento aumenta bastante.
+- Tenha cuidado com : YAGNI ( you aren`t gonna need it)
+KISS ( Keep it simple, stupid !)
+
+*/
