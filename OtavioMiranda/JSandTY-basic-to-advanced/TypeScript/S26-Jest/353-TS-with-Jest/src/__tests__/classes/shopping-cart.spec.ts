7ea4320fd782d2c8b22e53c3c19b6da75ebd9b59
˜LucasMdct˜ <lucasmedeiroscosta07@gmail.com>
2024-01-10 12:14:26 -0300

    Update TS Class 361 - Testing Shopping-Cart

---
+++ OtavioMiranda/JSandTY-basic-to-advanced/TypeScript/S26-Jest/353-TS-with-Jest/src/__tests__/classes/shopping-cart.spec.ts
@@ -0,0 +1,69 @@
+import { Discount } from '../../classes/discount';
+import { CartItem } from '../../classes/interfaces/cart-item';
+import { ShoppingCart } from '../../classes/shopping-cart';
+
+const createSut = () => {
+  const discountMock = createDiscountMock();
+  const sut = new ShoppingCart(discountMock);
+  return { sut, discountMock };
+};
+
+const createDiscountMock = () => {
+  class DiscountMock extends Discount {}
+  return new DiscountMock();
+};
+
+const createCartItem = (name: string, price: number) => {
+  class CartItemMock implements CartItem {
+    constructor(
+      public name: string,
+      public price: number,
+    ) {}
+  }
+
+  return new CartItemMock(name, price);
+};
+
+const createSutWithProducts = () => {
+  const { sut, discountMock } = createSut();
+  const cartItem1 = createCartItem('Lapis', 1.25);
+  const cartItem2 = createCartItem('Caneta', 1.89);
+  sut.addItem(cartItem1);
+  sut.addItem(cartItem2);
+  return { sut, discountMock };
+};
+
+describe('ShoppingCart', () => {
+  it('should be an empty cart when no product is added', () => {
+    const { sut } = createSut();
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should have 2 cart items', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+  });
+
+  it('should test total and total with discount', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.total()).toBe(3.14);
+    expect(sut.totalDiscount()).toBe(3.14);
+  });
+
+  it('should and products and clear cart', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.clear();
+    expect(sut.items.length).toBe(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should remove products', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.removeItem(1);
+    expect(sut.items.length).toBe(1);
+    sut.removeItem(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+});
