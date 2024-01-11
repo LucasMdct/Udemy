05b528bb629b5ebc8c94e03304b68140fbd7146b
˜LucasMdct˜ <lucasmedeiroscosta07@gmail.com>
2024-01-10 22:56:37 -0300

    Update TS Class 368 - useInterval ( REACT HOOK )

--- OtavioMiranda/JSandTY-basic-to-advanced/TypeScript/S27-React-Pomodoro/365-pomodoro/src/App.tsx
+++ OtavioMiranda/JSandTY-basic-to-advanced/TypeScript/S27-React-Pomodoro/365-pomodoro/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { PomodoroTimer } from './components/pomodoro-timer';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Olá jovems.</p>
+        <PomodoroTimer defaultPomodoTime={2000} />
       </header>
     </div>
   );
---
+++ OtavioMiranda/JSandTY-basic-to-advanced/TypeScript/S27-React-Pomodoro/365-pomodoro/src/components/pomodoro-timer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useInterval } from '../hooks/use-interval';
+
+interface Props {
+  defaultPomodoTime: number;
+}
+
+export function PomodoroTimer(props: Props): JSX.Element {
+  const [mainTime, setMainTime] = React.useState(props.defaultPomodoTime);
+
+  useInterval(() => {
+    setMainTime(mainTime - 1);
+  }, 1000);
+
+  return <div> lucas {mainTime}</div>;
+}
